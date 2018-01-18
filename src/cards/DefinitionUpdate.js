4595af744a8aeecadaf4e155cbb72619e0cd781b
SarahKersting <34747023+SarahKersting@users.noreply.github.com>
2018-01-18 22:15:19 +0100

    Update DefinitionUpdate.js

--- src/cards/DefinitionUpdate.js
+++ src/cards/DefinitionUpdate.js
@@ -16,7 +16,9 @@ const styles = theme => ({
 });
 
 const Intro = ({ classes }) => (
-  <CardTemplate headline="Hundkatzenschweinerein - Definition">
+  <CardTemplate headline="Hundkatzenschweinerein - Definition"
+  image="https://burst.shopifycdn.com/photos/black-brown-goat_925x.jpg"
+  >
     <Typography component="p" className={classes.paragraph}>
 Eine Hundkatzenschweinerei ist das Ergebnis einer erfolgreichen Hybridisierung von Organismen mittels gezielter Genomveränderung durch biochemische Methoden wie Crispr/Cas, wenn sie in einem nicht-institutionellen Rahmen erfolgt. Der Begriff kommt aus der deutschen do-it-yourself Biologie oder Biohacking Szene. Er ist dabei sowohl humorvoll gemeint, aber deutet auch die ethischen Probleme an, die durch das Erzeugen gentechnisch veränderter Hybride aufgeworfen werden.
     </Typography>
